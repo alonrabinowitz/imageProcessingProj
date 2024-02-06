031f0e387956017f6e2cad579f802e8b059e4707
SigmaAL <alon.freikorn@gmail.com>
2024-02-06 12:26:13 -0800

    Increase threshold

--- src/Filters/RandomKernelFilter.java
+++ src/Filters/RandomKernelFilter.java
@@ -14,7 +14,6 @@ public class RandomKernelFilter implements PixelFilter {
 
         short[][] reGrid = edgeKernel(grid, n);
         reGrid = thershold(reGrid);
-        reGrid = thinning(reGrid, n);
 
         short[][] reRed = edgeKernel(red, n);
         short[][] reGreen = edgeKernel(green, n);
@@ -38,86 +37,6 @@ public class RandomKernelFilter implements PixelFilter {
             }
         }
         return img;
-    }
-
-    public static short[][] thinning(short[][] img, int size){
-        short[][] returnImg = new short[img.length][img[0].length];
-
-        int[][] kernel = new int[size][size];
-        int[][] secKernel = new int[size][size];
-        int kernelWeight = 0;
-        int secKernelWeight = 0;
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                secKernel[i][j] = 999;
-                secKernel[0][size/2] = 0;
-                secKernel[0][size-1] = 0;
-                secKernel[size/2][size-1] = 0;
-                secKernel[size/2][0] = 255;
-                secKernel[size/2][size/2] = 255;
-                secKernel[size - 1][size/2] = 255;
-                secKernelWeight += secKernel[i][j];
-
-            }
-        }
-
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                kernel[i][j] = 999;
-                kernel[0][i] = 0;
-                kernel[size/2][size/2] = 255;
-                kernel[size-1][j] = 255;
-                kernelWeight += kernel[i][j];
-            }
-        }
-
-
-
-        for (int redo = 0; redo < 4; redo++) {
-
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    System.out.print(kernel[i][j] + "  ");
-                }
-                System.out.println();
-            }
-            System.out.println("======================================");
-//            for (int i = 0; i < size; i++) {
-//                for (int j = 0; j < size; j++) {
-//                    System.out.print(secKernel[i][j] + "  ");
-//                }
-//                System.out.println();
-//            }
-//            System.out.println("========================================");
-
-            for (int row = 0; row < img.length - size; row++) {
-                for (int col = 0; col < img[row].length - size; col++) {
-                    int output = 0;
-
-                    for (int i = 0; i < size; i++) {
-                        for (int j = 0; j < size; j++) {
-                            int kernelVal = kernel[i][j];
-                            int secKernelVal = secKernel[i][j];
-                            int pixVal = img[i + row][j + col];
-                            output += kernelVal * secKernelVal * pixVal;
-                        }
-                    }
-                    if (kernelWeight != 0) {
-                        output /= kernelWeight;
-                    } else output /= 1;
-
-
-                    if (output < 0) output = 0;
-                    if (output > 255) output = 255;
-                    returnImg[row + (size / 2)][col + size / 2] = (short) output;
-                }
-            }
-            kernel = rotate(kernel, size);
-            secKernel = rotate(kernel, size);
-        }
-        return returnImg;
     }
 
     public static short[][] edgeKernel(short[][] array, int size){

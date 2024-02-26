cbe2a4e7f1598cb6cb5ffb87638d1feb8d80f747
alonrabinowitz <62042858+alonrabinowitz@users.noreply.github.com>
2024-02-26 10:11:43 -0800

    Perfectly working

--- src/RunMe.java
+++ src/RunMe.java
@@ -3,9 +3,9 @@ import core.DisplayWindow;
 public class RunMe {
     public static void main(String[] args) {
         // --== Load an image to filter ==--
-        DisplayWindow.showFor("images/jugglingTestImage6.png", 800, 600);
+//        DisplayWindow.showFor("images/jugglingTestImage6.png", 800, 600);
 
         // --== Determine your input interactively with menus ==--
-//        DisplayWindow.getInputInteractively(800,600);
+        DisplayWindow.getInputInteractively(800,600);
     }
 }
